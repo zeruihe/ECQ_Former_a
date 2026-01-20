7ce357c8c77e2c98612ac265d67a86f000f78367
zeruihe <zeruuihe@163.com>
2026-01-20 17:14:54 +0800

    fix bertscore threshold

--- utils/vqa_metrics.py
+++ utils/vqa_metrics.py
@@ -204,7 +204,7 @@ def compute_vqa_metrics(
     golds: Iterable[str],
     is_closed: Iterable[bool],
     use_clean: bool = True,
-    bertscore_threshold: float = 0.90,
+    bertscore_threshold: float = 0.95,
 ) -> VQAMetrics:
     """Compute VQA metrics using hybrid semantic matching strategy.
     
